c83280b01b9d0b7f90b6cf4a4f160bd7defbd0a2
Channah <code.challenge.cv@gmail.com>
2022-08-20 18:05:02 +0100

    removed inline onclick and added event listener

--- script.js
+++ script.js
@@ -3,42 +3,25 @@
 
 // Solution 1 - Vanilla JavaScript using regex // Email validation
 
-// // function captureUserInput() {
-// //   let userInput = document.getElementById('email').value;
-// //   let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-// //   let emptyString = /^\s*$/;
-// //   let userInputField = document.getElementById('email');
-// //   let inputError = document.getElementById('email__error');
-
-// //   if (userInput.match(mailformat)) {
-// //     inputError.innerHTML = 'Thank you for registering!';
-// //     userInputField.focus();
-// //     return true;
-// //   } else if (userInput.match(emptyString)) {
-// //     inputError.innerHTML = 'Oops! Please add your email';
-// //   } else {
-// //     inputError.innerHTML = 'Oops! Please check your email';
-// //     userInputField.focus();
-// //     return false;
-// //   }
-// // }
-
-// Solution 2 - validator.js !important, validator.js only validates strings, it can also be used for sanitisation
-
-import validator from 'validator';
+let emailButton = document.getElementById('email__button');
+emailButton.addEventListener('click', captureUserInput);
 
 function captureUserInput() {
   let userInput = document.getElementById('email').value;
+  let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  let emptyString = /^\s*$/;
   let userInputField = document.getElementById('email');
   let inputError = document.getElementById('email__error');
 
-  // Check if name or about field is empty
-  if (validator.isEmpty(userInput)) {
+  if (userInput.match(mailformat)) {
+    inputError.innerHTML = 'Thank you for registering!';
+    userInputField.focus();
+    return true;
+  } else if (userInput.match(emptyString)) {
     inputError.innerHTML = 'Oops! Please add your email';
-  }
-
-  // Check if email format is correct
-  if (!validator.isEmail(userInput)) {
+  } else {
     inputError.innerHTML = 'Oops! Please check your email';
+    userInputField.focus();
+    return false;
   }
 }
