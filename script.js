fd9636fa0c1361937a6bbf4329d83202e07bfaa8
Channah <code.challenge.cv@gmail.com>
2022-08-22 08:55:51 +0100

    Notes added in Js file about better implementation

---
+++ script.js
@@ -0,0 +1,35 @@
+//One should not rely only upon JavaScript validation. JavaScript can easily be disabled.
+//Server side validation should also be performed.
+
+//One of the best methods to verify an email address is to send an email to that
+//email address with a unique token and ask the user to visit a certain URL on your website and enter that token.
+
+//Data sanitisation should be also performed both on the client and server side to minimise the risk of cross scripting attacks.
+
+//Vanilla JavaScript using regex // Email validation using regex solution
+
+let emailButton = document.getElementById('email__button');
+emailButton.addEventListener('click', captureUserInput);
+
+function captureUserInput() {
+  let userInput = document.getElementById('email').value;
+  let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  // alternative regex including thoughts about the drawbacks of using regexes:
+  // ([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|"([]!#-[^-~ \t]|(\\[\t -~]))+")@([0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?(\.[0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?)*|\[((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|IPv6:((((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){6}|::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){5}|[0-9A-Fa-f]{0,4}::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){4}|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):)?(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){3}|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,2}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){2}|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,3}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,4}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::)((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3})|(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3})|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,5}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3})|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,6}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::)|(?!IPv6:)[0-9A-Za-z-]*[0-9A-Za-z]:[!-Z^-~]+)])
+  // https://www.abstractapi.com/guides/email-address-pattern-validation
+  let emptyString = /^\s*$/g;
+  let userInputField = document.getElementById('email');
+  let inputError = document.getElementById('email__error');
+
+  if (userInput.match(mailformat)) {
+    inputError.innerHTML = 'Thank you for registering!';
+    userInputField.focus();
+    return true;
+  } else if (userInput.match(emptyString)) {
+    inputError.innerHTML = 'Oops! Please add your email';
+  } else {
+    inputError.innerHTML = 'Oops! Please check your email';
+    userInputField.focus();
+    return false;
+  }
+}
